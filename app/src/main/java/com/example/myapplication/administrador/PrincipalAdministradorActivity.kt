9bc461ee847fa21fa9fcf9439b1492979337e185
JoseJimenezGonzalez <josexisco1993@gmail.com>
2024-02-02 20:29:45 +0100

    commit ok

--- app/src/main/java/com/example/myapplication/administrador/PrincipalAdministradorActivity.kt
+++ app/src/main/java/com/example/myapplication/administrador/PrincipalAdministradorActivity.kt
@@ -1,12 +1,35 @@
 package com.example.myapplication.administrador
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityPrincipalAdministradorBinding
 
 class PrincipalAdministradorActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPrincipalAdministradorBinding
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_principal_administrador)
+
+        binding = ActivityPrincipalAdministradorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initUI()
+    }
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_administrador) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavigationViewAdministrador.setupWithNavController(navController)
     }
 }
