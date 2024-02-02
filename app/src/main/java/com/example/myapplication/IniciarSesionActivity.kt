02caad25eb05569534f9017b271c65d0cca9810e
JoseJimenezGonzalez <josexisco1993@gmail.com>
2024-02-02 20:01:17 +0100

    commit ok

--- app/src/main/java/com/example/myapplication/IniciarSesionActivity.kt
+++ app/src/main/java/com/example/myapplication/IniciarSesionActivity.kt
@@ -3,7 +3,10 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.myapplication.administrador.PrincipalAdministradorActivity
 import com.example.myapplication.databinding.ActivityIniciarSesionBinding
+import com.example.myapplication.usuario.PrincipalUsuarioActivity
 
 class IniciarSesionActivity : AppCompatActivity() {
 
@@ -17,6 +20,25 @@ class IniciarSesionActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         configurarBotonRegistroUsuario()
+
+        configurarInicioSesion()
+
+    }
+
+    private fun configurarInicioSesion() {
+        binding.btnIniciarSesion.setOnClickListener {
+            val correo = binding.tietCorreo.text.toString()
+            val password = binding.tietPassword.text.toString()
+            if(correo == "usuario" && password == "usuario"){
+                val intent = Intent(this, PrincipalUsuarioActivity::class.java)
+                startActivity(intent)
+            }else if(correo == "administrador" && password == "administrador"){
+                val intent = Intent(this, PrincipalAdministradorActivity::class.java)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, "Incorrecto", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun configurarBotonRegistroUsuario() {
