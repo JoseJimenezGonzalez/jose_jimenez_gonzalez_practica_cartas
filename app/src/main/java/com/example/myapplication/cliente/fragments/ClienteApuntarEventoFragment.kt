7df67f9d21df048136ef4f504b8298d6c5185d61
JoseJimenezGonzalez <josexisco1993@gmail.com>
2024-02-07 18:58:52 +0100

    commit ok

--- app/src/main/java/com/example/myapplication/cliente/fragments/ClienteApuntarEventoFragment.kt
+++ app/src/main/java/com/example/myapplication/cliente/fragments/ClienteApuntarEventoFragment.kt
@@ -5,15 +5,71 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.myapplication.R
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.administrador.model.adapter.rv.AdaptadorEventoAdministrador
+import com.example.myapplication.data.model.Evento
+import com.example.myapplication.databinding.FragmentClienteApuntarEventoBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class ClienteApuntarEventoFragment : Fragment() {
+
+    private var _binding: FragmentClienteApuntarEventoBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var recycler: RecyclerView
+    private  lateinit var lista:MutableList<Evento>
+    private lateinit var adaptador: AdaptadorEventoAdministrador
+    @Inject
+    lateinit var dbRef: DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cliente_apuntar_evento, container, false)
+        _binding = FragmentClienteApuntarEventoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //Codigo
+        configurarRecyclerView()
+
+    }
+    private fun configurarRecyclerView() {
+        lista = mutableListOf()
+        dbRef.child("tienda").child("eventos").addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                lista.clear()
+                snapshot.children.forEach{hijo: DataSnapshot? ->
+                    val pojoEvento = hijo?.getValue(Evento::class.java)
+                    lista.add(pojoEvento!!)
+                }
+                recycler.adapter?.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                println(error.message)
+            }
+        })
+
+        //
+        adaptador = AdaptadorEventoAdministrador(lista, findNavController())
+        apply {
+            recycler = binding.rvMostrarEventos
+            recycler.adapter = adaptador
+            recycler.layoutManager = LinearLayoutManager(context)
+            recycler.setHasFixedSize(true)
+        }
+
     }
 }
--- app/src/main/java/com/example/myapplication/cliente/fragments/ClienteVerEventoFragment.kt
+++ app/src/main/java/com/example/myapplication/cliente/fragments/ClienteVerEventoFragment.kt
@@ -5,16 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentClienteVerEventoBinding
 
 class ClienteVerEventoFragment : Fragment() {
 
+    private var _binding: FragmentClienteVerEventoBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cliente_ver_evento, container, false)
+        _binding = FragmentClienteVerEventoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //Codigo
+
     }
 }
