fe45d257838646b38c7f7ae916fc21f982a9104a
JoseJimenezGonzalez <josexisco1993@gmail.com>
2024-02-10 18:34:46 +0100

    commit ok

--- app/src/main/java/com/example/myapplication/administrador/fragments/AdministradorGestionarAjustesFragment.kt
+++ app/src/main/java/com/example/myapplication/administrador/fragments/AdministradorGestionarAjustesFragment.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication.cliente.fragments
+package com.example.myapplication.administrador.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.myapplication.R
 
-class ClienteBuscarCartaFragment : Fragment() {
+class AdministradorGestionarAjustesFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cliente_buscar_carta, container, false)
+        return inflater.inflate(R.layout.fragment_administrador_gestionar_ajustes, container, false)
     }
 
 }
