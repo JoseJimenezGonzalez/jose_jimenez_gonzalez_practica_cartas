f4174ef3a3bb5ceb80217a7d285a30d051dbaf7e
JoseJimenezGonzalez <josexisco1993@gmail.com>
2024-02-21 14:41:44 +0100

    commit ok

--- app/src/main/java/com/example/myapplication/RegistrarUsuarioActivity.kt
+++ app/src/main/java/com/example/myapplication/RegistrarUsuarioActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.util.Patterns
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -10,8 +11,11 @@ import com.example.myapplication.databinding.ActivityRegistrarUsuarioBinding
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class RegistrarUsuarioActivity : AppCompatActivity() {
 
@@ -21,14 +25,20 @@ class RegistrarUsuarioActivity : AppCompatActivity() {
 
     lateinit var dbRef: DatabaseReference
 
+    lateinit var nombreUsuariosAplicacion: MutableList<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityRegistrarUsuarioBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        nombreUsuariosAplicacion = mutableListOf()
+
         // Initialize Firebase Auth
         auth = Firebase.auth
+
+        obtenerNombreUsuariosAplicacion()
 
         configurarBotonIniciarSesion()
 
@@ -38,11 +48,11 @@ class RegistrarUsuarioActivity : AppCompatActivity() {
     private fun configurarBotonRegistrarse() {
         binding.btnRegistrarUsuario.setOnClickListener {
             //Primero nos traemos todos los datos del usuario
-            val nombre = binding.tietNombre.text.toString()
-            val apellidos = binding.tietApellidos.text.toString()
-            val email = binding.tietCorreo.text.toString()
-            val password = binding.tietPassword.text.toString()
-            val repeatPassword = binding.tietRepetirPassword.text.toString()
+            val nombre = binding.tietNombre.text.toString().trim()
+            val apellidos = binding.tietApellidos.text.toString().trim()
+            val email = binding.tietCorreo.text.toString().trim()
+            val password = binding.tietPassword.text.toString().trim()
+            val repeatPassword = binding.tietRepetirPassword.text.toString().trim()
             //Declaramos los chivatos de estas variables
             var esNombreCorrecto = false
             var esApellidoCorrecto = false
@@ -101,22 +111,32 @@ class RegistrarUsuarioActivity : AppCompatActivity() {
             }
 
             if(esNombreCorrecto && esApellidoCorrecto && esEmailCorrecto && sonPasswordIguales && tienePasswordLongitudCorrecta && tieneRepetirPasswordLongitudCorrecta){
-                auth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this) { task ->
-                        if (task.isSuccessful) {
-                            // Registro ok
-                            Toast.makeText(baseContext, "Registro completado.", Toast.LENGTH_SHORT,).show()
-                            //Vamos a registrarlo en nuetra base de datos
-                            val user = auth.currentUser
-                            val idUsuario = user?.uid
-                            val esAdministrador = esUsuarioAdministrador(email)
-                            val tipoUsuario = devolverTipoDeUsuario(esAdministrador)
-                            registrarEnBaseDeDatos(idUsuario, nombre, apellidos, email, password, tipoUsuario)
-                        } else {
-                            // Ha fallado el registro
-                            Toast.makeText(baseContext, "Hay una cuenta con ese email", Toast.LENGTH_SHORT,).show()
+                val nombreCompleto = "$nombre $apellidos".lowercase()
+                val existeUsuario = existeUsuario(nombreCompleto)
+                //Logs
+                Log.e("Nombre teclado", nombreCompleto)
+                Log.e("Lista usuarios", nombreUsuariosAplicacion.toString())
+                if(existeUsuario){
+                    Toast.makeText(this, "Ya existe un usuario con ese nombre", Toast.LENGTH_SHORT).show()
+                }else{
+                    auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this) { task ->
+                            if (task.isSuccessful) {
+                                // Registro ok
+                                Toast.makeText(baseContext, "Registro completado.", Toast.LENGTH_SHORT,).show()
+                                //Vamos a registrarlo en nuetra base de datos
+                                val user = auth.currentUser
+                                val idUsuario = user?.uid
+                                val esAdministrador = esUsuarioAdministrador(email)
+                                val tipoUsuario = devolverTipoDeUsuario(esAdministrador)
+                                registrarEnBaseDeDatos(idUsuario, nombre, apellidos, email, password, tipoUsuario)
+                            } else {
+                                // Ha fallado el registro
+                                Toast.makeText(baseContext, "Hay una cuenta con ese email", Toast.LENGTH_SHORT,).show()
+                            }
                         }
-                    }
+                }
+
             }
         }
     }
@@ -134,6 +154,32 @@ class RegistrarUsuarioActivity : AppCompatActivity() {
             )
         )
     }
+
+    private fun obtenerNombreUsuariosAplicacion(){
+        dbRef = FirebaseDatabase.getInstance().reference
+        dbRef.child("tienda").child("usuarios").addListenerForSingleValueEvent(
+            object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    nombreUsuariosAplicacion.clear()
+                    snapshot.children.forEach { usuario->
+                        val pojoUsuario = usuario.getValue(Usuario::class.java)
+                        val nombre = pojoUsuario!!.nombreUsuario
+                        val apellidos = pojoUsuario.apellidosUsuario
+                        val nombreCompleto = "$nombre $apellidos".lowercase()
+                        Log.e("Nombre usuario", nombreCompleto)
+                        nombreUsuariosAplicacion.add(nombreCompleto)
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    //Nada
+                }
+
+            }
+        )
+    }
+
+    private fun existeUsuario(nombreCompleto: String): Boolean = nombreCompleto in nombreUsuariosAplicacion
 
     private fun configurarBotonIniciarSesion() {
         binding.tvVolverIniciarSesion.setOnClickListener {
